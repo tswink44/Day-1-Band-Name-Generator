7648893ec27b56f1c5fdb4cfc6fce2500e2e634b
Tyler Swink <tswink4@gmail.com>
2023-09-07 20:33:09 -0400

    Day 1 Challenge Complete

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?")
+pet_name = input("What is the name of your pet?")
+band_name = city_name + " " + pet_name
+print("Your band name could be " + band_name)
